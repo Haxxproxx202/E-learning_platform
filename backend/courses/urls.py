20d7f9200c8844de2487c04bfb865222c3ba93a0
Haxxproxx202 <pjanecki88@gmail.com>
2023-05-25 22:27:10 +0200

    formset for modules

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CourseCreateView, ManageCourseListView, CourseUpdateView, CourseDeleteView
+from .views import CourseCreateView, ManageCourseListView, CourseUpdateView, CourseDeleteView, CourseModuleUpdateView
 
 app_name = 'courses'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create/', CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
+    path('<pk>/module/', CourseModuleUpdateView.as_view(), name='course_module_update')
 ]
