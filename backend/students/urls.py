9711b25647d451bdeddb000eb5fe3a457a85b3fd
Haxxproxx202 <pjanecki88@gmail.com>
2023-06-01 20:52:49 +0200

    course list and detail view

--- backend/students/urls.py
+++ backend/students/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 # from django.conf.urls import url
 
-# app_name = 'students'
+app_name = 'students'
 
 urlpatterns = [
     path('register/', views.StudentRegistrationView.as_view(), name='student_registration'),
+    path('enroll-course/', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
 ]
--- backend/students/views.py
+++ backend/students/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from braces.views import LoginRequiredMixin
+from django.views.generic.list import ListView
+from courses.models import Course
+
+from .forms import CourseEnrollForm
 
 
 class StudentRegistrationView(CreateView):
@@ -16,3 +21,27 @@ class StudentRegistrationView(CreateView):
         user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super(StudentEnrollCourseView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseListView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
