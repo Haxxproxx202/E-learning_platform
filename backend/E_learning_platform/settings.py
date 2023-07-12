1e06666bb89b8e57818f7dcfd1d10fb4e077f56b
Haxxproxx202 <pjanecki88@gmail.com>
2023-07-12 22:06:37 +0200

    Group communication channels with Redis

--- backend/E_learning_platform/settings.py
+++ backend/E_learning_platform/settings.py
@@ -161,3 +161,13 @@ REST_FRAMEWORK = {
     ]
 }
 ASGI_APPLICATION = "E_learning_platform.routing.application"
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
