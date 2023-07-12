04a8d7f7e7cf38fab44acd5084370cffeaf0586f
Haxxproxx202 <pjanecki88@gmail.com>
2023-07-12 22:28:07 +0200

    ChatConsumer class upgrading

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,10 +1,12 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = 'chat_%s' % self.id
         async_to_sync(self.channel_layer.group_add)(
@@ -14,12 +16,27 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
         if text_data:
             text_data_json = json.loads(text_data)
             message = text_data_json['message']
-            self.send(text_data=json.dumps({'message': message}))
+            now = timezone.now()
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'user': self.user.username,
+                    'datetime': now.isoformat()
+                }
+            )
         else:
             pass
+
+    def chat_message(self, event):
+        self.send(text_data=json.dumps(event))
